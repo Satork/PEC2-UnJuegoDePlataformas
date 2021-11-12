1517659616c11dda182eb7da4f738007983ca9fa
KSatorG <jmarrero3499@gmail.com>
2021-11-12 20:35:12 +0000

    Update WinController.cs

--- Assets/Scripts/WinController.cs
+++ Assets/Scripts/WinController.cs
@@ -1,21 +1,31 @@
 ﻿using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinController : MonoBehaviour {
 
 	public Transform flag;
 	public Transform downFlagPos;
-
+	public Canvas canvas;
+	
 	public float downSpeed = 1f;
 
 	private bool _isWin;
 
+	private void Awake() {
+		canvas.enabled = false;
+	}
+
 	private void Update() {
 		if (!_isWin) return;
 		// TODO Add total score + time
-		// TODO Add Win Screen
 		flag.position = Vector3.MoveTowards(flag.position, downFlagPos.position, downSpeed * Time.deltaTime);
+		if (flag.position.Equals(downFlagPos.position)) {
+			_isWin = false;
+			StartCoroutine(WinCoroutine());
+		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D contact) {
@@ -23,4 +33,11 @@ public class WinController : MonoBehaviour {
 		_isWin = true;
 	}
 
+	private IEnumerator WinCoroutine() {
+		canvas.enabled = true;
+		while (!Input.anyKey){
+			yield return null;
+		}
+		SceneManager.LoadScene("Game");
+	}
 }
