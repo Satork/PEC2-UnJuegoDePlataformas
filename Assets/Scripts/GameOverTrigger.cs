04829ac2a904a022abbf74e6296ffaa9786e9286
KSatorG <jmarrero3499@gmail.com>
2021-11-12 19:14:39 +0000

    GameOver screen added
    Added Game Over screen. Reloads scene when the 'R' key is pressed.

--- Assets/Scripts/GameOverTrigger.cs
+++ Assets/Scripts/GameOverTrigger.cs
@@ -1,13 +1,22 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverTrigger : MonoBehaviour {
 
 	public GameObject player;
+	public Canvas canvas;
 
 	private Animator _animator;
 	
+	private bool _isDead;
+	
 	private static readonly int IsDead = Animator.StringToHash("isDead");
+
+	private void Awake() {
+		canvas.enabled = false;
+	}
 
 	private void Start() {
 		_animator = player.GetComponent<Animator>();
@@ -15,7 +24,16 @@ public class GameOverTrigger : MonoBehaviour {
 
 	private void Update() {
 		if(!_animator.GetBool(IsDead)) return;
+		if (_isDead) return;
+		_isDead = true;
+		StartCoroutine(GameOverCoroutine());
+	}
 
+	IEnumerator GameOverCoroutine() {
+		canvas.enabled = true;
+		while (!Input.GetKeyDown(KeyCode.R)) {
+			yield return null;
+		}
 		SceneManager.LoadScene("Game");
 	}
 }
